594dec26930010f8ac1af1162e7c12a6f738c806
lawrenceteo <lawrence9729@gmail.com>
2025-12-22 10:59:44 +0800

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -2,11 +2,11 @@ import Lake
 open Lake DSL
 
 package ToyModel {
-  dependencies := #[{
-    name := `mathlib,
-    src := Source.git "https://github.com/leanprover-community/mathlib4.git" "master"
-  }]
+  -- No extra config needed
 }
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 @[default_target]
 lean_lib ToyModel {
