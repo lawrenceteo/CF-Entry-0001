8c18659fc3fa3957ed190952e2dca7d10a39abf8
lawrenceteo <lawrence9729@gmail.com>
2025-12-22 09:26:59 +0800

    Create lakefile.lean

---
+++ lakefile.lean
@@ -0,0 +1,11 @@
+import Lake
+open Lake DSL
+
+package CFEntry0001 {
+  -- add package configuration here if needed
+}
+
+@[default_target]
+lean_lib CFEntry0001 {
+  -- root module for your proofs
+}
